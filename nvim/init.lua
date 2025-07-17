998864929ffbf9c2cfcee9ee697fc3eb35436fea
Jared Givens <jargivens@tesla.com>
2025-07-17 14:38:46 -0700

    just do stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,21 +8,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     --local capabilities = require('blink.cmp').get_lsp_capabilities(config.cappabilties);
-    if client:supports_method('textDocument/diagnostic') then
-      vim.api.nvim_set_keymap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-    end
-    if client:supports_method('textDocument/definition') then
-      vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-    end
+    vim.api.nvim_set_keymap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
 
     -- Check if the LSP server supports document_formatting
-    if client:supports_method('textDocument/formatting') then
-      vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
-      vim.keymap.set('n', 'gF', function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-      end)
-    end
+    vim.api.nvim_buf_set_option(bufnr, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+    vim.keymap.set('n', 'gF', function()
+        vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+    end)
     -- grn   vim.lsp.buf.code_action
     -- gd    vim.lsp.buf.definition
     -- gri   vim.lsp.buf.implementation
